22ccfcecff0c3b3c3ea9d08f8bfb5f8e0a9c7fc2
rzinke <rzinke@jpl.caltech.edu>
2025-08-25 17:48:36 -0700

    Unit functions started

---
+++ src/riser/units.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Rob Zinke
+# (c) 2025 all rights reserved
+
+
+# Constants
+
+
+# Import modules
+
+
+#################### UNIT SCALING ####################
+
+
+# end of file
