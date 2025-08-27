280c15b1cdb6ad26adaf8f33c52df9c1f830f35c
rzinke <rzinke@jpl.caltech.edu>
2025-08-27 15:40:09 -0700

    added constants file with confidence limits

---
+++ src/riser/constants.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# Rob Zinke
+# (c) 2025 all rights reserved
+
+
+# Fraction of values within confidence limits of a normal distribution
+Psigma = {
+    '1': 0.682689492137086,
+    '2': 0.954499736103642,
+    '3': 0.997300203936740,
+    '4': 0.999936657516334,
+    '5': 0.999999426696856,
+    '6': 0.999999998026825,
+    '7': 0.999999999997440,
+}
+
+
+# end of file
