219b087fe8a48a230728287f7fd31882e37195aa
rzinke <rzinke@jpl.caltech.edu>
2025-09-03 12:38:27 -0700

    Warning added for reading markers in order

--- src/riser/markers/readers.py
+++ src/riser/markers/readers.py
@@ -11,7 +11,7 @@ import warnings
 
 import toml
 
-from riser.probability_functions import readers as pdf_readers
+from riser.probability_functions import readers as pdf_readers, analytics
 from riser.markers import DatedMarker
 from riser import units
 
@@ -34,13 +34,29 @@ def set_metadata_priority(metadata_item:str, file_item:str, spec_item:str):
 
 
 def initialize_marker_from_files(age_fname:str, displacement_fname:str,
-                                 verbose=False, **kwargs) -> DatedMarker:
+        verbose=False, **kwargs) -> DatedMarker:
     """
+    Metadata can be specified either in the PDF file itself, or in the config
+    toml file. Precedence is given to metadata specified in the PDF file if
+    there is a conflict, per the set_metadata_priority function.
+
+    Args    age_fname - str, name of age PDF file
+            displacement_fname - str, name of displacement PDF file
+            kwargs - dict, metadata for marker and age, displacement PDFs
+                name - str, marker name
+                age_name - str, descriptive name of age PDF
+                age_variable_type - str, age variable type (e.g., age)
+                age_unit - str, age physical unit
+                displacement_name - str, descriptive name of displacement PDF
+                displacement_variable_type - str, displacement variable type
+                    (e.g., displacement)
+                displacement_unit - str, displacement physical unit
+    Returns marker - DatedMarker
     """
     # Read age PDF
     age = pdf_readers.read_pdf(age_fname)
 
-    # Check age metadata
+    # Gather age metadata - precedence given to metadata in PDF file
     age.name = set_metadata_priority(
             "Age name", age.name,
             kwargs.get('age_name'))
@@ -53,7 +69,7 @@ def initialize_marker_from_files(age_fname:str, displacement_fname:str,
     # Read displacement PDF
     displacement = pdf_readers.read_pdf(displacement_fname)
 
-    # Check displacement metadata
+    # Gather displacement metadata - precedence given to metadata in PDF file
     displacement.name = set_metadata_priority(
             "Displacement name", displacement.name,
             kwargs.get('displacement_name'))
@@ -64,7 +80,7 @@ def initialize_marker_from_files(age_fname:str, displacement_fname:str,
             displacement.unit, kwargs.get('displacement_unit'))
 
     # Form age-displacement data into DatedMarker
-    marker = DatedMarker(age, displacement)
+    marker = DatedMarker(age, displacement, name=kwargs.get('name'))
 
     # Report if requested
     if verbose == True:
@@ -77,9 +93,9 @@ def read_markers_from_config(fname:str, verbose=False) -> dict:
     """Read marker data from a TOML configuration file.
     The file should have one [marker_name] entry per marker, and each marker
     should have entries for "age file" and "displacement file".
-    Optionally, age and displacement metadata can be specified, though any
-    metadata encoded in the file on disk will take precedence over the
-    user specification, per the set_metadata_priority function.
+    Optionally, age and displacement metadata can be specified in the config
+    toml file, though any metadata encoded in the PDF file takes precedence
+    over the config file, per the set_metadata_priority function.
 
     Args    fname - str, configuration file name.
     Returns markers - dict with one DatedMarker per marker name entry
@@ -111,6 +127,7 @@ def read_markers_from_config(fname:str, verbose=False) -> dict:
 
         # Initialize marker
         marker_args = {
+            'name': marker_name,
             'age_fname': age_fname,
             'displacement_fname': displacement_fname,
             'age_name': marker_spec.get('age name'),
@@ -125,6 +142,31 @@ def read_markers_from_config(fname:str, verbose=False) -> dict:
 
         # Write marker to dictionary
         markers[marker_name] = marker
+
+    # Check that markers are ordered youngest/smallest to oldest/largest
+    for i, marker in enumerate(markers.values()):
+        if i > 0:
+            # Compute reference age/displacement
+            ref_age = analytics.pdf_mean(ref_marker.age)
+            ref_disp = analytics.pdf_mean(ref_marker.displacement)
+
+            # Compute marker age/displacement
+            marker_age = analytics.pdf_mean(marker.age)
+            marker_disp = analytics.pdf_mean(marker.displacement)
+
+            # Check that marker is older/larger than previous
+            if marker_age < ref_age:
+                warnings.warn(f"Marker {marker.name} appears to be younger "
+                              f"than {ref_marker.name}. "
+                              f"Confirm marker order.", stacklevel=3)
+
+            if marker_disp < ref_disp:
+                warnings.warn(f"Marker {marker.name} appears to be less "
+                              f"displaced than {ref_marker.name}. "
+                              f"Confirm marker order.", stacklevel=3)
+
+        # Update reference marker
+        ref_marker = marker
 
     return markers
 
