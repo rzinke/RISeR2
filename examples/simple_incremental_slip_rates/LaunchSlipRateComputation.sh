3b96208199d29be920441de15e50d1703eb291e5
Robert Zinke <rzinke@Roberts-MacBook-Pro.local>
2025-09-16 17:43:14 -0700

    Added incremental slip rate examples

---
+++ examples/simple_incremental_slip_rates/LaunchSlipRateComputation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Compute an incremental fault slip rate based on a series of dated
+# displacement markers.
+
+
+view_displacement_age_history.py marker_config.toml \
+    --age-unit-out ky --show-labels \
+    -v
+
+compute_slip_rates.py marker_config.toml \
+    --age-unit-out y --displacement-unit-out mm \
+    --limit-positive --max-rate 6 \
+    -v -p -o slip_rate
