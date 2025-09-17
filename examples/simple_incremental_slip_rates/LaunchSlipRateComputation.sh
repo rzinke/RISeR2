fdaec5aa3e09b6757a0b29fd5dc2e6de72bddb38
rzinke <rzinke@jpl.caltech.edu>
2025-09-16 19:07:43 -0700

    Renamed simple slip rate example

--- examples/simple_incremental_slip_rates/LaunchSlipRateComputation.sh
+++ examples/simple_incremental_slip_rates/LaunchSlipRateComputation.sh
@@ -10,4 +10,4 @@ view_displacement_age_history.py marker_config.toml \
 compute_slip_rates.py marker_config.toml \
     --age-unit-out y --displacement-unit-out mm \
     --limit-positive --max-rate 12 \
-    -v -p -o slip_rate
+    -v -p -o SimpleExample
