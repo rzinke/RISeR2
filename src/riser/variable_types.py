153ddf31b23ca8c73276559aac389fccbc9ed316
rzinke <robert.zinke@gfz.de>
2026-07-29 23:45:54 +0200

    Simplified units and variable types.

--- src/riser/variable_types.py
+++ src/riser/variable_types.py
@@ -5,14 +5,14 @@
 
 """
 Because most RISeR2 functions are general and not specific to slip rates,
-variable types should be suggested but not enforced.
+variable types should be suggested but not enforced except in the case
+of slip rate calculations.
 """
 
 # Public API
 __all__ = [
     "SUPPORTED_VARIABLE_TYPES",
     "check_variable_type_supported",
-    "check_same_pdf_variable_types",
 ]
 
 
@@ -27,21 +27,19 @@ SUPPORTED_VARIABLE_TYPES = (
 # Import modules
 import warnings
 
-from .probability_functions import PDF
+
+#################### VARIABLE TYPE TYPE ####################
+type VariableType = str
 
 
 #################### PDF VARIABLE TYPE CHECKS ####################
-def check_variable_type_supported(
-    variable_type: str, throw_error: bool = False, verbose: bool = False
-) -> bool:
+def check_variable_type_supported(variable_type: str):
     """Check whether the specified variable type is supported.
 
     Parameters
     ----------
     variable_type : str
         Specified variable type.
-    throw_error : bool
-        Throw an error if the variable type is not supported.
     
     Returns
     -------
@@ -49,53 +47,8 @@ def check_variable_type_supported(
         True if variable type is supported.
     """
     # Check if variable type is supported
-    if variable_type in SUPPORTED_VARIABLE_TYPES:
-        # Confirm support
-        if verbose:
-            print(f"Variable type '{variable_type}' is known and supported")
-
-        return True
-
-    else:
-        # Variable type is not supported
-        if throw_error:
-            # Throw error
-            raise ValueError(f"Variable type '{variable_type}' not supported")
-
-        else:
-            # Warn only
-            warnings.warn(
-                f"Variable type '{variable_type}' not supported",
-                stacklevel=2,
-            )
-
-            return False
-
-
-def check_same_pdf_variable_types(pdfs: list[PDF]) -> str | None:
-    """Check that the variable types are the same among a series of PDFs.
-
-    Parameters
-    ----------
-    pdfs : list[PDF]
-        PDFs to check.
-
-    Returns
-    -------
-    variable type : str or None
-        Common variable type, if variable type is common to all PDFs.
-    """
-    # Establish initial variable type
-    variable_type = pdfs[0].variable_type
-
-    # Loop through all PDFs
-    for pdf in pdfs[1:]:
-        # Check PDF variable type against initial
-        if pdf.variable_type != variable_type:
-            # Nullify variable type
-            variable_type = None
-
-    return variable_type
+    if not variable_type in SUPPORTED_VARIABLE_TYPES:
+        raise ValueError(f"Variable type '{variable_type}' not supported")
 
 
 # end of file
