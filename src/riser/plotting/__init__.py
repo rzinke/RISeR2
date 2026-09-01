b4591ab3c2d158315293f2e2cac2f59be84a9f01
rzinke <robert.zinke@gfz.de>
2026-09-01 06:19:25 +0200

    Plotting init reorganization.

--- src/riser/plotting/__init__.py
+++ src/riser/plotting/__init__.py
@@ -8,26 +8,38 @@ Plotting functions.
 """
 
 # Import modules
-from .pdf_plots import *
-from .cdf_plots import *
-from .variable_pair_plots import *
-from .filter_plots import *
-from .mc_plots import *
-
-
-# Public API
 from . import pdf_plots
 from . import cdf_plots
 from . import variable_pair_plots
 from . import filter_plots
 from . import mc_plots
 
+# Public API
 __all__ = (
-    pdf_plots.__all__
-    + cdf_plots.__all__
-    + variable_pair_plots.__all__
-    + filter_plots.__all__
-    + mc_plots.__all__
+    # PDF plots
+    "axis_label_from_pdf",
+    "axis_label_from_pdfs",
+    "plot_pdf_line",
+    "plot_pdf_filled",
+    "plot_pdf_labeled",
+    "plot_pdf_confidence_range",
+    "plot_pdf_stack",
+    # CDF plots
+    "plot_cdf_line",
+    "plot_cdf_filled",
+    "plot_cdf_labeled",
+    # Variable pair plots
+    "plot_variable_pair_whisker",
+    "plot_variable_pairs_whisker",
+    "plot_variable_pair_rectangle",
+    "plot_variable_pairs_rectangle",
+    "plot_variable_pairs_joint_pdf",
+    "get_markers_plot",
+    "plot_variable_pairs",
+    # Monte Carlo plots
+    "plot_mc_picks",
+    # Filter plots
+    "plot_filter_kernel",
 )
 
 
