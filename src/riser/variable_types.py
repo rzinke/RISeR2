ee0dbe5a702a704dcdc47f1ae34a06539e827664
rzinke <robert.zinke@gfz.de>
2026-09-01 13:24:41 +0200

    Variable types checker now accepts None as input.

--- src/riser/variable_types.py
+++ src/riser/variable_types.py
@@ -32,21 +32,38 @@ type VariableType = str
 
 
 #################### PDF VARIABLE TYPE CHECKS ####################
-def check_variable_type_supported(variable_type: str) -> None:
+def check_variable_type_supported(variable_type: str | None) -> None:
     """Check whether the specified variable type is supported.
+
+    Warns if variable type is `None`.
+    Silent if variable type is valid.
+    Raises if variable type is not supported.
 
     Parameters
     ----------
-    variable_type : str
+    variable_type : str or None
         Specified variable type.
     
     Returns
     -------
     None
     """
+    # Warn if base unit is None
+    if variable_type is None:
+        warnings.warn(
+            f"Variable type 'None' not specified. "
+            f"It is strongly suggested to specify variable type, "
+            f"e.g., {', '.join(SUPPORTED_VARIABLE_TYPES)}"
+        )
+
+        return
+
     # Check if variable type is supported
     if not variable_type in SUPPORTED_VARIABLE_TYPES:
-        raise ValueError(f"Variable type '{variable_type}' not supported")
+        raise ValueError(
+            f"Variable type '{variable_type}' not supported. "
+            f"Use one of {', '.join(SUPPORTED_VARIABLE_TYPES)}"
+        )
 
 
 # end of file
