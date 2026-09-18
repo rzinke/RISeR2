b3f17fbb6cab622cc5fc828a2cf158f9762591a6
rzinke <48299159+rzinke@users.noreply.github.com>
2026-09-18 15:08:22 +0200

    Update weight_functions.py

--- src/riser/probability_functions/weight_functions.py
+++ src/riser/probability_functions/weight_functions.py
@@ -32,8 +32,22 @@ class WeightFunction:
         self.x = x
         self.px = px
 
-    def area(self):
+    def area(self) -> float:
+        """Compute the area under the curve of the weight function.
+
+        The meaning of the area is context-dependent.
+
+        Returns
+        -------
+        area : float
+            Area under the curve of the weight function.
+        """
         return integration.integrate(x=self.x, px=self.px)
+
+    def normalize(self, name: str, variable_type: str, unit: str) -> PDF:
+        """
+        """
+        return
 
 
 # end of file
