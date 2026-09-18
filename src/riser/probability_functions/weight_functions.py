dda63887a2d10df832258b33fc5595328d81f5da
rzinke <48299159+rzinke@users.noreply.github.com>
2026-09-18 14:51:20 +0200

    Update weight_functions.py

--- src/riser/probability_functions/weight_functions.py
+++ src/riser/probability_functions/weight_functions.py
@@ -1,1 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2025, 2026 Robert Zinke. Licensed under the MIT License.
 
+
+# Public API
+__all__ = [
+    "WeightFunction",
+]
+
+
+# Import modules
+import numpy as np
+
+from .. import integration
+
+
+#################### WEIGHT FUNCTION CLASS ####################
+class WeightFunction:
+    ...
+
+
+# end of file
