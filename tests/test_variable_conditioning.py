6e457a5633a98b1935168a4cd5757ae254648e9f
rzinke <robert.zinke@gfz.de>
2026-09-23 09:40:02 +0200

    Removed defunct metadata tests from condition.core.

--- tests/test_variable_conditioning.py
+++ tests/test_variable_conditioning.py
@@ -37,33 +37,21 @@ class TestCondition:
         with pytest.raises(ValueError, match="The size of the weighting array"):
             var_fcns.condition.core.condition(prior, weight)
 
-    @pytest.mark.parametrize(
-        "prior_name, vartype, unit, post_name",
-        [
-            (None, None, None, None),
-            ("X", "age", "y", "Xpost"),
-        ],
-    )
-    def test_metadata_consistent(self, prior_name, vartype, unit, post_name):
+    def test_metadata_consistent(self):
         x = PDFs.value_arrays.precise_array(0.0, 1.0, 0.1)
         px = PDFs.parametric_functions.uniform(x=x, a=0.0, b=1.0)
+
         prior = PDFs.PDF(
-            x=x,
-            px=px,
-            name=prior_name,
-            variable_type=vartype,
-            unit=unit,
+            x=x, px=px, name="X1", variable_type="age", unit="y"
         )
 
         weight = 1 - x
 
-        posterior, _ = var_fcns.condition.core.condition(
-            prior, weight, name=post_name
-        )
+        posterior, _ = var_fcns.condition.core.condition(prior, weight)
 
-        assert posterior.name == post_name
-        assert posterior.variable_type == vartype
-        assert posterior.unit == unit
+        assert posterior.name is None
+        assert posterior.variable_type is None
+        assert posterior.unit is None
 
 
 # end of file
