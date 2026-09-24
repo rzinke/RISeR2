4af0bf7d68e04cb47deb5386869d1ea70b253702
rzinke <robert.zinke@gfz.de>
2026-09-24 15:27:51 +0200

    Finished tests for pdf formation.

--- tests/test_sampling_mc.py
+++ tests/test_sampling_mc.py
@@ -21,36 +21,7 @@ class TestSampleMonteCarlo:
     def test_all_pass(self):
         """Trivial case in which no samples are rejected.
         """
-
-
-class TestSamplesToPdfHistogram:
-    def test_minimal(self):
-        mu = 1.0
-        sigma = 2.0
-
-        samples = np.random.normal(mu, sigma, 1_000_000)
-
-        pdf = sampling.pdf_formation.samples_to_pdf_histogram(
-            samples=samples,
-        )
-
-        assert PDFs.analytics.pdf_mean(pdf) == pytest.approx(mu)
-        assert PDFs.analytics.pdf_std(pdf) == pytest.approx(sigma)
-
-
-class TestSamplesToPdfKde:
-    def test_minimal(self):
-        mu = 1.0
-        sigma = 2.0
-
-        samples = np.random.normal(mu, sigma, 1_000)
-
-        pdf = sampling.pdf_formation.samples_to_pdf_kde(
-            samples=samples,
-        )
-
-        assert PDFs.analytics.pdf_mean(pdf) == pytest.approx(mu)
-        assert PDFs.analytics.pdf_std(pdf) == pytest.approx(sigma)
+        ...
 
 
 # end of file
--- tests/test_sampling_pdf_formation.py
+++ tests/test_sampling_pdf_formation.py
@@ -21,30 +21,46 @@ class TestSamplesToPdfHistogram:
     def test_minimal(self):
         mu = 1.0
         sigma = 2.0
+        n_samples = 1_000_000
 
-        samples = np.random.normal(mu, sigma, 1_000_000)
+        samples = np.random.normal(mu, sigma, n_samples)
 
         pdf = sampling.pdf_formation.samples_to_pdf_histogram(
             samples=samples,
         )
 
-        assert PDFs.analytics.pdf_mean(pdf) == pytest.approx(mu)
-        assert PDFs.analytics.pdf_std(pdf) == pytest.approx(sigma)
+        se_mean = sigma / np.sqrt(n_samples)
+        assert (
+            PDFs.analytics.pdf_mean(pdf) == pytest.approx(mu, abs=2 * se_mean)
+        )
+
+        se_std = sigma / np.sqrt(2 * n_samples)
+        assert (
+            PDFs.analytics.pdf_std(pdf) == pytest.approx(sigma, abs=2 * se_std)
+        )
 
 
 class TestSamplesToPdfKde:
     def test_minimal(self):
         mu = 1.0
         sigma = 2.0
+        n_samples = 1_000
 
-        samples = np.random.normal(mu, sigma, 1_000)
+        samples = np.random.normal(mu, sigma, n_samples)
 
         pdf = sampling.pdf_formation.samples_to_pdf_kde(
             samples=samples,
         )
 
-        assert PDFs.analytics.pdf_mean(pdf) == pytest.approx(mu)
-        assert PDFs.analytics.pdf_std(pdf) == pytest.approx(sigma)
+        se_mean = sigma / np.sqrt(n_samples)
+        assert (
+            PDFs.analytics.pdf_mean(pdf) == pytest.approx(mu, abs=2 * se_mean)
+        )
+
+        se_std = sigma / np.sqrt(2 * n_samples)
+        assert (
+            PDFs.analytics.pdf_std(pdf) == pytest.approx(sigma, abs=2 * se_std)
+        )
 
 
 # end of file
